d78d3662a03b3f7feb60489baa23cc3a6124ea3d
Zoya-Sk <sakinask2006@email.com>
2026-04-06 11:31:03 +0530

    fix: typo in socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -4,7 +4,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const server = http.createServer(app);
-const io = new Server(server, {
+const io = new server(server, {
   cors: {
     origin: [
       "https://smart-x-front-end.vercel.app",
